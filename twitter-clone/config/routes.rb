781f8b55b5af5ce31f4077da46bc2ef856e0bd38
sfinkenhdz <sarah.finken@gmail.com>
2016-08-22 14:28:01 -0500

    Set up routes

--- twitter-clone/config/routes.rb
+++ twitter-clone/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :tweets
+  resources :users
+  resources :followings
+  resources :hashtags
+
+  root to: 'tweets#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
