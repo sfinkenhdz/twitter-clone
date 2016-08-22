068731e5620589e48c298c2b936d911822aff9cf
sfinkenhdz <sarah.finken@gmail.com>
2016-08-22 17:06:42 -0500

    Add validations

--- twitter-clone/app/models/hashtag.rb
+++ twitter-clone/app/models/hashtag.rb
@@ -1,3 +1,4 @@
 class Hashtag < ActiveRecord::Base
+  validates_presence_of :name
   has_many :taggings
 end
--- twitter-clone/app/models/tweet.rb
+++ twitter-clone/app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ActiveRecord::Base
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  validates :content, presence: true
+  validates :content, length: { maximum: 140 }
   belongs_to :creator, class_name: "User"
   has_many :taggings
   has_many :hashtags, through: :taggings
