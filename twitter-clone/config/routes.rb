398e2f3f6106416d64eebbfe4d5e6bf76feea51f
sfinkenhdz <sarah.finken@gmail.com>
2016-08-22 15:16:27 -0500

    Add login, logout functionality

--- twitter-clone/config/routes.rb
+++ twitter-clone/config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 
   root to: 'tweets#index'
 
+  resources :sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
