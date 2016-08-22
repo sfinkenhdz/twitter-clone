615fddbd49901f1c00b57f7ccb8b8f9c8d137565
sfinkenhdz <sarah.finken@gmail.com>
2016-08-22 16:21:24 -0500

    Add image upload functionality

--- twitter-clone/app/models/user.rb
+++ twitter-clone/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
