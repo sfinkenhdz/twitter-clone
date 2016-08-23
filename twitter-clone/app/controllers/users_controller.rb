c502e1d8494ff9d477277da41c5160df822c102c
sfinkenhdz <sarah.finken@gmail.com>
2016-08-22 19:10:19 -0500

    Style profile pics

--- twitter-clone/app/controllers/users_controller.rb
+++ twitter-clone/app/controllers/users_controller.rb
@@ -17,30 +17,25 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to tweets_path
-      else
-        render :new
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to tweets_path
+    else
+      render :new
     end
   end
 
   def update
-      if @user.update(user_params)
-        redirect_to @user
-
-      else
-        render :edit
-      end
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
   def destroy
     @user.destroy
-      redirect_to tweets_path
-    end
+    redirect_to tweets_path
   end
 
   private
